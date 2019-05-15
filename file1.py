76c99c9681ad6771f4b3c8fae0ac3b6f72ab38ea
priyankasakthivelan <50616146+priyankasakthivelan@users.noreply.github.com>
2019-05-15 10:06:55 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,5 +1,9 @@
 num = int(input("enter a number: "))
-if num > 0: print("Positive ")
-elif num == 0: print("Zero")
-elif num < 0: print("Negative ")
-  else: print("invalid input")
+if num > 0: 
+  print("Positive ")
+elif num == 0: 
+  print("Zero")
+elif num < 0: 
+  print("Negative ")
+else: 
+  print("invalid input")
