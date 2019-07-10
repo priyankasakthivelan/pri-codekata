ce63fc7098d69226b3f3299588ad7a3c3d7580d1
priyankasakthivelan <50616146+priyankasakthivelan@users.noreply.github.com>
2019-07-10 20:58:10 +0530

    Add files via upload

--- p1.py
+++ p1.py
@@ -1,17 +1,6 @@
-p=int(input())
-q=[]
-for i in range(p):
-    q.append(input())
-r=q[0]
-s,a=0,0
-n=""
-for i in range(len(r)):
-    a=0
-    for k in range(1,p):
-          if r[i]==q[k][i]:
-            a=a+1
-    if a==p-1:
-       n=n+r[i]
-    else:
-        break
-print(n)
+p=input()
+l=len(p)
+s=""
+for i in range (l):
+  s+=p[l-i-1]
+print (s)
